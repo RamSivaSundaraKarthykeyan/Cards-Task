689c462f1d02142d67a9a65a86e80a36949bf654
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-03-24 20:47:16 +0530

    IDk what is improved in this version but it seems like the text field works better now

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,13 +1,17 @@
 import "../CSS/checkBox.css";
 
-function CheckBox({ id, removeCheckBox }) {
+function CheckBox({ id, removeCheckBox, updatedText }) {
   return (
     <>
       <label>
         <div>
           <input type="checkbox" />
           {id}.
-          <input type="text" className="textField" />
+          <input
+            type="text"
+            className="textField"
+            onChange={(e) => updatedText(e.target.value)}
+          />
           <button onClick={removeCheckBox}>-</button>
         </div>
       </label>
@@ -16,3 +20,7 @@ function CheckBox({ id, removeCheckBox }) {
 }
 
 export default CheckBox;
+
+// e is the event object which provides the informaution about the event, such as the type of event, the element that triggered the event and its current value
+// e.target refers to the element that triggered the event. In this case it is <input> element
+//e.target.value accesses the current value of the target
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -3,16 +3,32 @@ import CheckBox from "./CheckBox.jsx";
 
 function TaskCard() {
   const [checkBoxes, setCheckBoxes] = useState([]);
+  const [text, setText] = useState([""]); // the text state is an array that holds the text each checkBox
 
   const addCheckBox = () => {
     const newId =
       checkBoxes.length === 0 ? 1 : checkBoxes[checkBoxes.length - 1].id + 1;
     setCheckBoxes([...checkBoxes, { id: newId }]);
+    setText([...text, ""]); // the spread operator ... copies the current "text" array and appends a new empty string""
   };
 
   const deleteCheckBox = (id) => {
     const updatedCheckBox = checkBoxes.filter((cb) => cb.id !== id);
     setCheckBoxes(updatedCheckBox);
+
+    // It is a comman convention in JS to indicate that the parameter is unused,
+    //  we use it here because we dont actually need the value of the element so it is used as a placeholder
+    // "_" represents the current element of the array during iteration, "index" it is the index of the element in the array
+    const updatedText = text.filter((_, index) => index !== id - 1);
+    setText(updatedText);
+  };
+
+  const updatedText = (id, value) => {
+    setText((prevText) => {
+      const updated = [...prevText];
+      updated[id - 1] = value;
+      return updated;
+    });
   };
 
   return (
@@ -23,6 +39,7 @@ function TaskCard() {
           id={cb.id}
           key={cb.id}
           removeCheckBox={() => deleteCheckBox(cb.id)}
+          updatedText={(value) => updatedText(cb.id, value)}
         />
       ))}
       <button onClick={addCheckBox}>+</button>
