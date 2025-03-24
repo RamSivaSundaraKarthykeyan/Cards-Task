93bc2f16858a502c592d4070512272eb26ce232d
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-03-24 20:48:44 +0530

    IDk what is improved in this version but it seems like the text field works better now

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -24,3 +24,4 @@ export default CheckBox;
 // e is the event object which provides the informaution about the event, such as the type of event, the element that triggered the event and its current value
 // e.target refers to the element that triggered the event. In this case it is <input> element
 //e.target.value accesses the current value of the target
+// No changes at all
