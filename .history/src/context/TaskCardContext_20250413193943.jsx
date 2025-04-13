1300948f209e81a6800a99d6c9fefbc49e2f79c1
RamSivaSundaraKarthykeyan <srssk2005@gmail.com>
2025-04-13 19:44:12 +0530

    Implemented TaskCardContext code but not sure whether it is working

---
+++ .history/src/context/TaskCardContext_20250413193943.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from "react";
+
+const TaskCardContext = createContext();
+
+export function TaskCardProvider({ children }) {
+  const [taskCards, setTaskCards] = useState([]);
+
+  const addTaskCard = (taskCard) => setTaskCards((prev) => [...prev, taskCard]);
+  const removeTaskCard = (id) =>
+    setTaskCards((prev) => prev.filter((tc) => tc.id !== id));
+
+  const pickRandomTaskCard = () => {
+    if (taskCards.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * taskCards.length);
+    return taskCards[randomIndex];
+    };
+    
+    return (
+        <TaskCarfContext.Provider value = {{ taskCards, addTaskCard, removeTaskCard, pickRandomRaskCard}}
+    )
+}
